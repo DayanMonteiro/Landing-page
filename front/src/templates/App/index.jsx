694c2d84cb19de84d62a0baef1a653c047ecd50e
DayanMonteiro <dayan.monteiro@fdte.io>
2022-11-10 12:54:33 -0300

    initial commit

---
+++ front/src/templates/App/index.jsx
@@ -0,0 +1,13 @@
+import { Heading } from "../../components/Heading";
+import * as S from "./styles";
+
+function Home() {
+  return (
+    <S.Wrapper background="blue">
+      {/* <h1>Hello </h1> */}
+      <Heading>Opa Teste!</Heading>
+    </S.Wrapper>
+  );
+}
+
+export default Home;
