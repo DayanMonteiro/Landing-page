e79a594173b449ea51f5be20cc2118c6a4729fa5
DayanMonteiro <dmsmamb@gmail.com>
2022-11-11 18:50:39 -0300

    NavLink

---
+++ front/src/components/NavLinks/index.jsx
@@ -0,0 +1,23 @@
+import P from "prop-types";
+import * as S from "./styles";
+import { MenuLink } from "../MenuLink";
+
+export const NavLinks = ({ links = [] }) => {
+  return (
+    <S.Container>
+      {links.map((link) => (
+        <MenuLink key={link.link} {...link} />
+      ))}
+    </S.Container>
+  );
+};
+
+NavLinks.propTypes = {
+  links: P.arrayOf(
+    P.shape({
+      children: P.string.isRequired,
+      link: P.string.isRequired,
+      newTab: P.bool,
+    })
+  ),
+};
